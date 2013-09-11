f1a5c209d3be485856039aeb1145152e0a77ef01
rexim <reximkut@gmail.com>
2013-09-11 11:52:39 +0700

    Finished fortune_to_sqlite3.py script

--- fortune_to_sqlite3.py
+++ fortune_to_sqlite3.py
@@ -4,37 +4,58 @@ import getopt, sys
 import sqlite3
 
 def usage():
-    pass
+    print 'fortune_to_sqlite3.py [-h] [-f <fortune-path>] [-d <database-path>]'
 
 def parse_args(args):
-    pass
+    opts, _ = getopt.getopt(args, "hf:d:", ["help", "fortune", "database"])
+    help_needed = False
+    fortune_path = None
+    database_path = None
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            help_needed = True
+        elif o in ("-f", "--fortune"):
+            fortune_path = a
+        elif o in ("-d", "--database"):
+            database_path = a
+        else:
+            assert False, "unhandled option"
+
+    help_needed = fortune_path == None or database_path == None
+
+    return help_needed, fortune_path, database_path
 
 def read_fortunes(fortunes_path):
     with open(fortunes_path) as f:
-        [fortune for fortune in f.read().split("\n%\n") if len(fortune) > 0]
+        return [fortune
+                for fortune in f.read().split("\n%\n")
+                if len(fortune) > 0]
 
 def create_schema_if_needed(db):
     db.execute('CREATE TABLE IF NOT EXISTS fortunes (body text)')
     db.commit()
 
 def add_fortunes(db, fortunes):
-    db.executemany('INSERT INTO fortunes VALUES (?)', fortunes)
+    db.executemany('INSERT INTO fortunes VALUES (?)',
+                   [(fortune,) for fortune in fortunes])
     db.commit()
 
-if __name__ == "__main__":
+def main():
     try:
-        (help_needed, fortunes_path, database_path) = parse_args(sys.argv[1:])
+        help_needed, fortunes_path, database_path = parse_args(sys.argv[1:])
     except getopt.GetoptError as err:
         print str(err)
         usage()
         sys.exit(2)
-    
 
     if help_needed:
         usage()
-        sys.exet(0)
+        sys.exit(0)
 
     with sqlite3.connect(database_path) as db:
         create_schema_if_needed(db)
-        fortunes = [(fortune,) for fortune in read_fortunes(fortunes_path)]
-        add_fortunes(db, fortubes)
+        add_fortunes(db, read_fortunes(fortunes_path))
+
+if __name__ == "__main__":
+    main()
