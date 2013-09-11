ffcd67d53bf66e43b235c1642ae06e77f1fc9a75
rexim <reximkut@gmail.com>
2013-09-11 12:25:13 +0700

    Finished fortune_service.py script.

--- fortune_service.py
+++ fortune_service.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
 from flask import Flask, Response
+import sqlite3
+import json
 
 app = Flask(__name__)
 
-@app.route("/fortune")
-def fortune():
-    return "Not implemented yet!"
+def get_random_fortune(db):
+    for row in db.execute('select * from fortunes order by random() limit 1'):
+        return row[0]
 
-if __name__ == "__main__":
+@app.route('/api/random')
+def fortune():
+    with sqlite3.connect('./fortune.db') as db:
+        return Response(json.dumps({'body': get_random_fortune(db)},
+                                   sort_keys=True,
+                                   indent=4,
+                                   separators=(',', ': ')),
+                        mimetype='application/json')
+
+if __name__ == '__main__':
+    app.debug = True
     app.run();
