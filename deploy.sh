6cf2842c9882fdce22f395e3134b7ab889ccd725
rexim <reximkut@gmail.com>
2013-09-14 20:40:38 +0400

    Added deployment script.

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cp ./config/production.cfg /etc/fortune_service.cfg
+
+cp ./fortune_service.py /var/www/fortune_service/fortune_service.py
+cp ./fortune_service.fcgi /var/www/fortune_service/fortune_service.fcgi
+
+if [ ! -f fortunes.db ]; then
+    ./build_db.sh
+fi
+
+cp ./fortunes.db /var/www/fortune_service/fortunes.db
+
+chown -R nginx:nginx /var/www/fortune_service/
