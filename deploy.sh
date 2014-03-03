dcccff72c7bccc27795ebec870997f86e3d960b4
rexim <reximkut@gmail.com>
2014-03-03 16:10:58 +0700

    Added fortune_schema module to the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ cp ./config/production.cfg /etc/fortune_service.cfg
 
 cp ./fortune_service.py /var/www/fortune_service/fortune_service.py
 cp ./fortune_service.fcgi /var/www/fortune_service/fortune_service.fcgi
+cp ./fortune_schema.py /var/www/fortune_service/fortune_schema.py
 
 if [ ! -f fortunes.db ]; then
     ./build_db.sh
